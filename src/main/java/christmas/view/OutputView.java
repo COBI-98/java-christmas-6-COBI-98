25fd087e9b512365a4c0fbe34bc1c0bca010bc64
COBI-98 <tkdgus968@naver.com>
2023-11-14 18:36:22 +0900

    feat: 메뉴판 출력뷰 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,9 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.MenuGroup;
+import java.util.List;
+
 public class OutputView {
 
     private static final String RESTAURANT_EVENT_INTRO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
@@ -10,5 +14,13 @@ public class OutputView {
 
     public static void printException(Exception exception) {
         System.out.println(exception.getMessage());
+    }
+
+    public static void printRestaurantMenu() {
+        List<MenuGroup> menuPanList = List.of(MenuGroup.APPETIZER, MenuGroup.MAIN, MenuGroup.DESSERT, MenuGroup.BEVERAGE);
+
+        menuPanList.stream()
+                .map(menuGroup -> menuGroup.toString() + "\n")
+                .forEach(System.out::println);
     }
 }
