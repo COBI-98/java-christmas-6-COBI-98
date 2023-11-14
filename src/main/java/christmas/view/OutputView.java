e39c4b7a651d6c8a8b64e4e0f7ac0aa1a27cabd3
COBI-98 <tkdgus968@naver.com>
2023-11-15 03:36:14 +0900

    feat: 예약 날짜 이벤트 미리보기 출력뷰 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,19 +1,17 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Date;
 import christmas.domain.MenuGroup;
 import java.util.List;
 
 public class OutputView {
 
     private static final String RESTAURANT_EVENT_INTRO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String RESTAURANT_EVENT_PREVIEW = "%s에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
     public static void printRestaurantIntro(){
         System.out.println(RESTAURANT_EVENT_INTRO);
-    }
-
-    public static void printException(Exception exception) {
-        System.out.println(exception.getMessage());
     }
 
     public static void printRestaurantMenu() {
@@ -22,5 +20,13 @@ public class OutputView {
         menuPanList.stream()
                 .map(menuGroup -> menuGroup.toString() + "\n")
                 .forEach(System.out::println);
+    }
+
+    public static void printRestaurantEventPreview(Date date) {
+        System.out.println(String.format(RESTAURANT_EVENT_PREVIEW, date.toString()));
+    }
+
+    public static void printException(Exception exception) {
+        System.out.println(exception.getMessage());
     }
 }
