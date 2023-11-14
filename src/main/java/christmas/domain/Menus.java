96bcf3b08876fa5e6784587fa3ec03639739cd06
COBI-98 <tkdgus968@naver.com>
2023-11-15 03:22:25 +0900

    feat: 중복메뉴를 입력한 경우 검증 구현

--- src/main/java/christmas/domain/Menus.java
+++ src/main/java/christmas/domain/Menus.java
@@ -1,6 +1,26 @@
 package christmas.domain;
 
+import static christmas.message.ErrorMessages.INVALID_ORDER_FORMAT;
+
+import christmas.message.ErrorMessages;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record Menus(List<Menu> menuList) {
+
+
+    public Menus {
+        validateDuplicateFromMenus(menuList);
+    }
+
+    private void validateDuplicateFromMenus(List<Menu> menuList) {
+        long distinctCount = menuList.stream()
+                .map(menu -> menu.getMenuName())
+                .distinct()
+                .count();
+
+        if (menuList.size() != distinctCount){
+            throw new IllegalArgumentException(INVALID_ORDER_FORMAT.getMessage());
+        }
+    }
 }
---
+++ src/test/java/christmas/domain/MenusTest.java
@@ -0,0 +1,31 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MenusTest {
+
+    @DisplayName("validateDuplicateFromMenus() : 중복메뉴를 입력한 경우")
+    @ParameterizedTest
+    @CsvSource({"양송이수프,양송이수프,2","시저샐러드,시저샐러드,1"})
+    void validateDuplicateFromMenus_fail(String menu1, String menu2, String quantity) throws Exception{
+        //given
+        String exceptionMessage = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+        List<Menu> menus = List.of(new Menu(menu1, quantity), new Menu(menu2, quantity));
+
+        //when //then
+        assertThatThrownBy(() -> new Menus(menus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(exceptionMessage);
+    }
+
+}
