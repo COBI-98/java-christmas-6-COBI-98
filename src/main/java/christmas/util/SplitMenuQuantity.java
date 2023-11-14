c6d051ef957130ea74c3e80f7aed0a11ea38b051
COBI-98 <tkdgus968@naver.com>
2023-11-15 00:39:56 +0900

    refactor: 메뉴리스트 생성 메서드 네임 리팩터링

--- src/main/java/christmas/util/SplitMenuQuantity.java
+++ src/main/java/christmas/util/SplitMenuQuantity.java
@@ -2,18 +2,14 @@ package christmas.util;
 
 import christmas.domain.Menu;
 import christmas.domain.MenuFormat;
-import christmas.domain.MenuName;
-import christmas.domain.MenuQuantity;
 import christmas.domain.Menus;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class SplitMenuQuantity {
 
     private static final String SPLIT_DELIMITER = "-";
 
-    public static Menus splitMenuQuantity(MenuFormat menuFormat) {
+    public static Menus createMenus(MenuFormat menuFormat) {
 
         return new Menus(menuFormat.menuFormat().stream()
                 .map(menu -> menu.split(SPLIT_DELIMITER))
