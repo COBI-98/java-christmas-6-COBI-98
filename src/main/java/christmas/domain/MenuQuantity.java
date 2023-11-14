2aac74cfbd8b85fd69baf9f80fca23280cbc8aab
COBI-98 <tkdgus968@naver.com>
2023-11-15 00:32:56 +0900

    feat: 메뉴개수 생성 및 검증 기능 구현

---
+++ src/main/java/christmas/domain/MenuQuantity.java
@@ -0,0 +1,43 @@
+package christmas.domain;
+
+import static christmas.message.ErrorMessages.INVALID_NUMBER_TYPE;
+import static christmas.message.ErrorMessages.INVALID_ORDER_FORMAT;
+
+import christmas.message.ErrorMessages;
+
+public record MenuQuantity(String quantity) {
+
+    private static final int MIN_QUANTITY = 1;
+    private static final int MAX_QUANTITY = 20;
+
+    public MenuQuantity {
+        validateEmptyFromMenuQuantity(quantity);
+        validateCharacterFromMenuQuantity(quantity);
+        validateRangeFromMenuQuantity(quantity);
+    }
+
+    private void validateEmptyFromMenuQuantity(String quantity) {
+        if (quantity.isEmpty()) {
+            throw new IllegalArgumentException(INVALID_ORDER_FORMAT.getMessage());
+        }
+    }
+
+    private void validateCharacterFromMenuQuantity(String quantity) {
+        for (int idx = 0; idx < quantity.length(); idx++) {
+            if (!Character.isDigit(quantity.charAt(idx))) {
+                throw new IllegalArgumentException(INVALID_ORDER_FORMAT.getMessage());
+            }
+        }
+    }
+
+    private void validateRangeFromMenuQuantity(String quantity){
+        int quantityNumber = convertToInt(quantity);
+        if (quantityNumber < MIN_QUANTITY || quantityNumber > MAX_QUANTITY){
+            throw new IllegalArgumentException(INVALID_ORDER_FORMAT.getMessage());
+        }
+    }
+
+    private static int convertToInt(String quantity) {
+        return Integer.parseInt(quantity);
+    }
+}
---
+++ src/test/java/christmas/domain/MenuQuantityTest.java
@@ -0,0 +1,63 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MenuQuantityTest {
+    
+    @DisplayName("constructor() : 메뉴 개수 생성")
+    @ParameterizedTest
+    @ValueSource(strings = {"1","5"})
+    void menu_quantity_success(String quantity) throws Exception{
+        //given
+        MenuQuantity menuQuantity = new MenuQuantity(quantity);
+
+        //when //then
+        assertThat(menuQuantity).isNotNull();
+    }
+
+    @DisplayName("validateEmptyFromMenuQuantity() : 수량을 체크하지 않은 경우")
+    @Test
+    void validateEmptyFromMenuQuantity_fail() throws Exception{
+        //given
+        String quantity = "";
+        String exceptionMessage = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+        //when //then
+        assertThatThrownBy(() -> new MenuQuantity(quantity))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(exceptionMessage);
+    }
+
+    @DisplayName("validateCharacterFromMenuQuantity() : 수량에 문자가 들어가는 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {" ","e","1e","1dollar","#"})
+    void validateCharacterFromMenuQuantity_fail(String quantity) throws Exception{
+        //given
+        String exceptionMessage = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+        //when //then
+        assertThatThrownBy(() -> new MenuQuantity(quantity))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(exceptionMessage);
+    }
+    
+    @DisplayName("validateRangeFromMenuQuantity() : 1이상 20이하의 수량을 체크하지 않는 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"0","21"})
+    void validateRangeFromMenuQuantity_fail(String quantity) throws Exception{
+        String exceptionMessage = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+        //when //then
+        assertThatThrownBy(() -> new MenuQuantity(quantity))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(exceptionMessage);
+    }
+}
