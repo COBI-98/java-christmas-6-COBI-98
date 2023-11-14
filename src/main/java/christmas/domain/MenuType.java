b95e423940f3eb802b2055f2791ca6883f229443
COBI-98 <tkdgus968@naver.com>
2023-11-14 18:34:42 +0900

    feat: 메뉴 타입 재정의

--- src/main/java/christmas/domain/MenuType.java
+++ src/main/java/christmas/domain/MenuType.java
@@ -1,12 +1,14 @@
 package christmas.domain;
 
+import java.text.DecimalFormat;
+
 public enum MenuType {
     BUTTON_MUSHROOM_SOUP("양송이수프", 6_000),
     TAPAS("타파스", 5_500),
     CAESAR_SALAD("시저샐러드", 8_000),
     T_BONE_STEAK("티본스테이크", 55_000),
     BARBECUE_RIBS("바비큐립", 54_000),
-    SEAFOOD_PASTA("해산물파스타",35_000),
+    SEAFOOD_PASTA("해산물파스타", 35_000),
     CHRISTMAS_PASTA("크리스마스파스타", 25_000),
     CHOCOLATE_CAKE("초코케이크", 15_000),
     ICE_CREAM("아이스크림", 5_000),
@@ -28,5 +30,15 @@ public enum MenuType {
 
     public int getPrice() {
         return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(%s)", title, formatPrice(price));
+    }
+
+    private String formatPrice(int price) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(price);
     }
 }
---
+++ src/test/java/christmas/domain/MenuTypeTest.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuTypeTest {
+
+    @DisplayName("toString() : 메뉴 타입 요구사항 재정의")
+    @Test
+    void menu_type_to_string_success() throws Exception{
+        //given
+        MenuType buttonMushroomSoup = MenuType.BUTTON_MUSHROOM_SOUP;
+
+        //when //then
+        assertThat(buttonMushroomSoup.toString()).isEqualTo("양송이수프(6,000)");
+    }
+}
