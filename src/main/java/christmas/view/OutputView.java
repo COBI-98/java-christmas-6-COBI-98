706e9efb303d07b0873109c56c621d941d475666
COBI-98 <tkdgus968@naver.com>
2023-11-14 17:16:01 +0900

    feat: 식당 이벤트 소개글 출력뷰 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class OutputView {
+
+    private static final String RESTAURANT_EVENT_INTRO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public static void printRestaurantIntro(){
+        System.out.println(RESTAURANT_EVENT_INTRO);
+    }
+}
