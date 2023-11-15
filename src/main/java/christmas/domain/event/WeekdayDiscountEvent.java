f1ff1b41140585a26b2c9dcbdf268626ddf26013
COBI-98 <tkdgus968@naver.com>
2023-11-15 16:11:49 +0900

    feat: 평일 디저트 할인 기능 구현

---
+++ src/main/java/christmas/domain/event/WeekdayDiscountEvent.java
@@ -0,0 +1,39 @@
+package christmas.domain.event;
+
+import christmas.domain.Date;
+import christmas.domain.MenuGroup;
+import christmas.domain.MenuType;
+import christmas.domain.Order;
+
+public final class WeekdayDiscountEvent implements Event {
+
+    private static final int CHRISTMAS_EVENT_MONTH = 12;
+    private static final int CHRISTMAS_EVENT_MIN_DAY = 1;
+    private static final int CHRISTMAS_EVENT_MAX_DAY = 31;
+    private static final int CHRISTMAS_EVENT_DESERT_DISCOUNT = 2_023;
+    private int benefit;
+
+    @Override
+    public boolean isApplicable(Date date) {
+        return date.getMonth() == CHRISTMAS_EVENT_MONTH &&
+                date.getDay() >= CHRISTMAS_EVENT_MIN_DAY &&
+                date.getDay() <= CHRISTMAS_EVENT_MAX_DAY &&
+                !date.isWeekend();
+    }
+
+    @Override
+    public int calculateDiscount(Date date, Order order) {
+        benefit = order.getMenus().menuList().stream()
+                .mapToInt(menu -> {
+                    MenuType menuType = MenuType.findByMenuName(menu.getMenuName());
+                    MenuGroup menuGroup = MenuGroup.findByMenuType(menuType);
+                    int menuQuantity = menu.getMenuQuantity();
+                    if (menuGroup == MenuGroup.DESSERT) {
+                        return CHRISTMAS_EVENT_DESERT_DISCOUNT * menuQuantity;
+                    }
+                    return 0;
+                })
+                .sum();
+        return benefit;
+    }
+}
---
+++ src/test/java/christmas/domain/event/WeekdayDiscountEventTest.java
@@ -0,0 +1,59 @@
+package christmas.domain.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.domain.Date;
+import christmas.domain.Menu;
+import christmas.domain.Menus;
+import christmas.domain.Order;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class WeekdayDiscountEventTest {
+
+    @DisplayName("isApplicable() : 평일이 아닌 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {8, 9, 15, 16})
+    void WeekdayDiscountEvent_isApplicable_fail(int day) throws Exception{
+        //given
+        Event weekdayDiscountEvent = new WeekdayDiscountEvent();
+        Date visitOfDate = new Date(day);
+
+        //when
+        boolean hasEventEffectiveDate = weekdayDiscountEvent.isApplicable(visitOfDate);
+
+        // then
+        assertThat(hasEventEffectiveDate).isFalse();
+    }
+
+    @DisplayName("calculateDiscount() : 크리스마스 평일 디저트 할인 금액 확인")
+    @ParameterizedTest
+    @CsvSource({"티본스테이크,2,초코케이크,3,6069", "초코케이크,1,아이스크림,4,10115"})
+    void WeekdayDiscountEvent_calculateDiscount_success(
+            String menu1, String quantity1,
+            String menu2, String quantity2, int discountAmount) throws Exception{
+        //given
+        int visitWeekDay = 4;
+        Event weekdayDiscountEvent = new WeekdayDiscountEvent();
+        Date visitOfDate = new Date(visitWeekDay);
+        Order order = createOrder(menu1, quantity1, menu2, quantity2);
+
+        //when
+        int discount = weekdayDiscountEvent.calculateDiscount(visitOfDate, order);
+
+        // then
+        assertThat(discount).isEqualTo(discountAmount);
+
+    }
+
+    private Order createOrder(String menu1, String quantity1, String menu2, String quantity2) {
+        Menus menus = new Menus(List.of(new Menu(menu1, quantity1),
+                new Menu(menu2, quantity2)));
+
+        return new Order(menus);
+    }
+}
